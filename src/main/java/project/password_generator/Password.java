6d6242664e5095917e366114984d1a20043f1e30
Matheson <mat.habbershaw@hotmail.com>
2022-01-20 20:04:58 -0500

    General functionality of the generator is complete.
    - Proper error warnings appear with specific input restrictions
    
    TODO:
    - General bug-fixing

--- src/main/java/project/password_generator/Password.java
+++ src/main/java/project/password_generator/Password.java
@@ -13,11 +13,166 @@ public class Password {
     final String UPPERCASE = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
     //input variables
+    private int length;
+    private boolean hasUppers;
+    private boolean hasNumbers;
+    private int upperMax;
+    private int numberMax;
 
-
-    //constructor
-
+    //constructors
+    public Password() {}
+    public Password(int length, boolean hasUppers, boolean hasNumbers) {
+        setLength(length);
+        setHasUppers(hasUppers);
+        setHasNumbers(hasNumbers);
+    }
+    public Password(int length, boolean hasUppers, boolean hasNumbers, int upperMax, int numberMax) {
+        setLength(length);
+        setHasUppers(hasUppers);
+        setHasNumbers(hasNumbers);
+        setUpperMax(upperMax);
+        setNumberMax(numberMax);
+    }
 
     //setter and getter
+    public int getLength() {
+        return length;
+    }
+    public void setLength(int length) {
+        if(length < 4) {
+            throw new IllegalArgumentException("Password length should be more than three (3).");
+        }
+        if(length > 30) {
+            throw new IllegalArgumentException("Password length should be a maximum of thirty (30) letters.");
+        }
+        this.length = length;
+    }
 
+    public boolean isUsingUppers() {return hasUppers;}
+    public void setHasUppers(boolean hasUppers) {
+        this.hasUppers = hasUppers;
+    }
+
+    public boolean isUsingNumbers() {return hasNumbers;}
+    public void setHasNumbers(boolean hasNumbers) {
+        this.hasNumbers = hasNumbers;
+    }
+
+    public int getUpperMax() {return upperMax;}
+    public void setUpperMax(int upperMax) {
+        if(isUsingUppers()) {
+            if(upperMax > getLength()){
+                throw new IllegalArgumentException("You want too many uppercase letters.  Try again.");
+            }
+            if(upperMax < 1) {
+                throw new IllegalArgumentException("Uppercase input can not be less than one (1).");
+            }
+            this.upperMax = upperMax;
+        } else { this.upperMax = 0; }
+    }
+
+    public int getNumberMax() {return numberMax;}
+    public void setNumberMax(int numberMax) {
+        if(isUsingNumbers()){
+            if(numberMax > getLength()) {
+                throw new IllegalArgumentException("You want too many numbers.  Try again.");
+            }
+            if(numberMax < 1) {
+                throw new IllegalArgumentException("Number input can not be less than one (1).");
+            }
+            this.numberMax = numberMax;
+        } else { this.numberMax = 0; }
+    }
+
+
+    public String passwordCreator(int wantedLength, int upCount, int numCount, String symbolString) {
+        StringBuilder pw = new StringBuilder();
+
+        boolean repeat = true;
+        while(repeat) {
+            pw.delete(0, wantedLength);
+            int numCounter = 0, upperCounter = 0, lowerCounter = 0, symbolCounter = 0;
+
+            for(int i=0; i<wantedLength; i++) {
+                int switchPick = secRandom.nextInt(3);  //randomize characterType
+                switch(switchPick) {
+                    case 0:
+                        //SYMBOLs
+                        if(symbolCounter < lowerCounter) {
+                            if(!symbolString.isEmpty()) {
+                                int res = random.nextInt(symbolString.length());
+                                char symbol = symbolString.charAt(res);
+
+                                pw.append(symbol);  //appending symbol
+                                symbolCounter++;
+                                break;
+                            }
+                        }
+                    case 1:
+                        //NUMBERs
+                        if(hasNumbers){
+                            if(numCounter < numCount && numCounter <= lowerCounter) {
+                                int res = random.nextInt(NUMBERSTRING.length());
+                                char number = NUMBERSTRING.charAt(res);
+
+                                pw.append(number);  //appending number
+                                numCounter++;
+                                break;
+                            }
+                        }
+                    case 2:
+                        //UPPERs
+                        if(hasUppers) {
+                            if(upperCounter < upCount && upperCounter <= lowerCounter) {
+                                int res = random.nextInt(UPPERCASE.length());
+                                char upperLetter = UPPERCASE.charAt(res);
+
+                                pw.append(upperLetter);  //appending Uppercase Letter
+                                upperCounter++;
+                                break;
+                            }
+                        }
+                    default:
+                        //LOWERs
+                        if(symbolString.isEmpty()) {
+                            int res = random.nextInt(LOWERCASE.length());
+                            char lowerLetter = LOWERCASE.charAt(res);
+
+                            pw.append(lowerLetter);
+                            lowerCounter++;
+                        } else {
+                            //if symbols wanted, add possibility to add symbol instead
+                            if(lowerCounter < (symbolCounter * 3)) {
+                                int res = random.nextInt(LOWERCASE.length());
+                                char lowerLetter = LOWERCASE.charAt(res);
+
+                                pw.append(lowerLetter);
+                                lowerCounter++;
+                            } else {
+                                int res = random.nextInt(symbolString.length());  //random int based on size of the symbolString
+                                char symbol = symbolString.charAt(res);
+
+                                pw.append(symbol);  //append a symbol
+                                symbolCounter++;
+                            }
+                        }
+                }
+            }
+            char securityCheck = pw.charAt(0);
+            if(securityCheck == '_' || securityCheck == '.' || securityCheck == '%' || securityCheck == '$' || securityCheck == '#' || securityCheck == '@' || securityCheck == '<') {
+                repeat = true;
+            } else { repeat = false; }
+        }
+        //if out of while loop -- return password
+        return pw.toString();
+    }
+
+    @Override
+    public String toString() {
+        return "length = " + length +
+                ", hasUppers = " + hasUppers +
+                ", hasNumbers = " + hasNumbers +
+                ", upperMax = " + upperMax +
+                ", numberMax = " + numberMax;
+    }
 }
--- src/main/java/project/password_generator/PasswordController.java
+++ src/main/java/project/password_generator/PasswordController.java
@@ -2,35 +2,205 @@ package project.password_generator;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.CheckBox;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
-public class PasswordController {
+import java.net.URL;
+import java.util.ResourceBundle;
 
+public class PasswordController implements Initializable {
+
+    @FXML
+    private Label errorString;
+    @FXML
+    private TextField displayString, maxNumCount, maxUpperCount, passwordLength;
     @FXML
     private CheckBox and, apostrophe, asteric, atSign, bSlash, backQuote, cBracket, cCurly, cParenthesis, carat, colon,
             comma, dbQuote, dollarSign, equals, exclamation, fSlash, greaterThan, hasNumbers, hasUppers, hashTag,
             lessThan, minus, oBracket, oCurly, oParenthesis, percent, period, pipe, plus, question, semiColon, tilde, underScore;
 
-    @FXML
-    private TextField displayString, maxNumCount, maxUpperCount, passwordLength;
 
+    private Password thePassword;
+    private CheckBox[] boxArray;
 
+    //on start..
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        System.out.println("APP STARTED ... ");
 
+        //instantiating password Obj and pre-populate
+        thePassword = new Password(5, true, true, 1, 1 );
+        passwordLength.setText(String.valueOf(thePassword.getLength()));
+        hasUppers.setSelected(thePassword.isUsingUppers());
+        hasNumbers.setSelected(thePassword.isUsingNumbers());
+        maxUpperCount.setText(String.valueOf(thePassword.getUpperMax()));
+        maxNumCount.setText(String.valueOf(thePassword.getNumberMax()));
 
-    @FXML
-    void deselectButton(ActionEvent event) {
-
+        //instantiating 1 box-array and populate.
+        boxArray = new CheckBox[32];
+        captureUserSymbols();
     }
+
 
     @FXML
     void generateButton(ActionEvent event) {
+        displayString.setText("");
+        errorString.setText("");
+
+        String symbolInstance = symbolStringCreator(boxArray);
+
+        //set if app is use uppers and/or numbers in calculation
+        thePassword.setHasUppers(hasUppers.isSelected());
+        thePassword.setHasNumbers(hasNumbers.isSelected());
+
+        //method to call respective setters and return number
+        try {
+            int length = captureNumber(passwordLength);
+            int upCount = captureNumber(maxUpperCount);
+            int numCount = captureNumber(maxNumCount);
+
+            String result = thePassword.passwordCreator(length, upCount, numCount, symbolInstance);
+            displayString.setText(result);
+
+            //DEBUGGING PRINT LINES
+            System.out.printf("SYMBOL_STRING:   %s%n", symbolInstance);
+            System.out.printf("PASSWORD_OBJ:   %s%n", thePassword.toString());
+            System.out.printf("RETURN:   %s%n%n", result);
+
+        } catch (Exception ex) {
+            errorString.setText(ex.getMessage());
+        }
+
+
 
     }
 
     @FXML
-    void selectButton(ActionEvent event) {
-
+    void deselectAllButton(ActionEvent event) {
+        //loop through the CheckBox[] -- set everything to false
+        for(CheckBox checkBox : boxArray) {
+            checkBox.setSelected(false);
+        }
     }
 
+    @FXML
+    void selectAllButton(ActionEvent event) {
+        //loop through the CheckBox[] -- set everything to true
+        for(CheckBox checkBox : boxArray) {
+            checkBox.setSelected(true);
+        }
+    }
+
+    private void captureUserSymbols() {
+        /*
+            - to be called on start to populate the boxArray
+        */
+        boxArray[0] = tilde;
+        boxArray[1] = backQuote;
+        boxArray[2] = exclamation;
+        boxArray[3] = atSign;
+        boxArray[4] = hashTag;
+        boxArray[5] = dollarSign;
+        boxArray[6] = percent;
+        boxArray[7] = carat;
+        boxArray[8] = and;
+        boxArray[9] = asteric;
+        boxArray[10] = oParenthesis;
+        boxArray[11] = cParenthesis;
+        boxArray[12] = underScore;
+        boxArray[13] = minus;
+        boxArray[14] = plus;
+        boxArray[15] = equals;
+        boxArray[16] = cBracket;
+        boxArray[17] = oBracket;
+        boxArray[18] = cCurly;
+        boxArray[19] = oCurly;
+        boxArray[20] = greaterThan;
+        boxArray[21] = lessThan;
+        boxArray[22] = question;
+        boxArray[23] = comma;
+        boxArray[24] = colon;
+        boxArray[25] = semiColon;
+        boxArray[26] = period;
+        boxArray[27] = pipe;
+        boxArray[28] = dbQuote;
+        boxArray[29] = bSlash;
+        boxArray[30] = fSlash;
+        boxArray[31] = apostrophe;
+    }  //method to return checkBox variables as checkboxArray
+
+    /**
+     * @param userInput
+     * used to filter out strings before calling appropriate setters.
+     *
+     * @exception NumberFormatException
+     * thrown if input is not found to be a number.
+     * */
+    private int captureNumber(TextField userInput) {
+        String inputType = userInput.getId();
+        switch(inputType) {
+            case "passwordLength":
+                if(userInput.getText().isEmpty()) {
+                    userInput.setText("0");
+                }
+                try {
+                    int len = Integer.parseInt(userInput.getText());
+                    thePassword.setLength(len);
+                } catch(NumberFormatException nfe) {
+                    throw new NumberFormatException("Password length must be a number.");
+                }
+                break;
+
+            case "maxUpperCount":
+                if(userInput.getText().isEmpty() && thePassword.isUsingUppers()) {
+                    userInput.setText("1");
+                }
+                if(userInput.getText().isEmpty() && !thePassword.isUsingUppers()) {
+                    userInput.setText("0");
+                }
+                try {
+                    int res = Integer.parseInt(userInput.getText());
+                    thePassword.setUpperMax(res);
+                } catch(NumberFormatException nfe) {
+                    throw new NumberFormatException("Uppercase limit must be a number.");
+                }
+                break;
+
+            case "maxNumCount":
+
+                if(userInput.getText().isEmpty() && thePassword.isUsingNumbers()) {
+                    userInput.setText("1");
+                }
+                if(userInput.getText().isEmpty() && !thePassword.isUsingNumbers()) {
+                    userInput.setText("0");
+                }
+                try {
+                    int res = Integer.parseInt(userInput.getText());
+                    thePassword.setNumberMax(res);
+                } catch(NumberFormatException fne) {
+                    throw new NumberFormatException("Number limit must be a number.");
+                }
+                break;
+        }
+        return Integer.parseInt(userInput.getText());
+    }
+
+    /**
+     * @return
+     * String of user-desired symbols
+    * */
+    private String symbolStringCreator(CheckBox[] theBoxArray) {
+
+        StringBuilder symbols = new StringBuilder();
+
+        for(CheckBox checkBox : theBoxArray) {
+            if(checkBox.isSelected()) {
+                symbols.append(checkBox.getText());
+            }
+        }
+
+        return symbols.toString();
+    }
 }
