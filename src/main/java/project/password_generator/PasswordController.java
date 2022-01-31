381c1b19eed0be11200dd3e5eeae5605407e1c65
Matheson <mat.habbershaw@hotmail.com>
2022-01-31 16:48:42 -0500

    General Update to PasswordController
    - TextFormatter used to briefly sanitize inputs of all alphabetized characters.
    - Created an integerFilter using a UnaryOperator that handles the TextFormatter based on an input change.
    - Changed max inputs from TextField to Spinners for general increase to usability.
    - These spinners are put through the TextFormatter to ensure integer inputs.
    
    TODO:
    - General update to Password.java model.

--- src/main/java/project/password_generator/PasswordController.java
+++ src/main/java/project/password_generator/PasswordController.java
@@ -3,78 +3,104 @@ package project.password_generator;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.CheckBox;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
+import javafx.util.converter.IntegerStringConverter;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.function.UnaryOperator;
 
 public class PasswordController implements Initializable {
 
     @FXML
     private Label errorString;
     @FXML
-    private TextField displayString, maxNumCount, maxUpperCount, passwordLength;
+    private TextField displayString, passwordLength ;
+    @FXML
+    private Spinner<Integer> maxNumCount, maxUpperCount;
     @FXML
     private CheckBox and, apostrophe, asteric, atSign, bSlash, backQuote, cBracket, cCurly, cParenthesis, carat, colon,
-            comma, dbQuote, dollarSign, equals, exclamation, fSlash, greaterThan, hasNumbers, hasUppers, hashTag,
+            comma, dbQuote, dollarSign, equals, exclamation, fSlash, greaterThan, includeNumbers, includeUppercase, hashTag,
             lessThan, minus, oBracket, oCurly, oParenthesis, percent, period, pipe, plus, question, semiColon, tilde, underScore;
 
 
     private Password thePassword;
     private CheckBox[] boxArray;
 
+
     //on start..
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         System.out.println("APP STARTED ... ");
 
-        //instantiating password Obj and pre-populate
-        thePassword = new Password(5, true, true, 1, 1 );
-        passwordLength.setText(String.valueOf(thePassword.getLength()));
-        hasUppers.setSelected(thePassword.isUsingUppers());
-        hasNumbers.setSelected(thePassword.isUsingNumbers());
-        maxUpperCount.setText(String.valueOf(thePassword.getUpperMax()));
-        maxNumCount.setText(String.valueOf(thePassword.getNumberMax()));
-
-        //instantiating 1 box-array and populate.
+        //instantiating 1 box-array -- populate.
         boxArray = new CheckBox[32];
         captureUserSymbols();
-    }
 
+        //Text-Formatter to be used on text-fields whose input must only be a number.
+        UnaryOperator<TextFormatter.Change> integerFilter = change -> {
+            String newText = change.getControlNewText();
+            if(newText.matches("([0-9][0-9]*)?")) {
+                return change;
+            }
+            //try-catch the text-fields. SpinnerFactory text-fields throw NullPointerException.
+            try {
+                switch(change.getControl().getId()){
+                    case "passwordLength":
+                        errorString.setText("Password length can not contain letters.");
+                        break;
+                    case "numSpinnerTextField":
+                        errorString.setText("Max-numbers must be a number.");
+                        break;
+                    case "uppSpinnerTextField":
+                        errorString.setText("Max-uppercase's must be a number.");
+                        break;
+                }
+            } catch(NullPointerException ex) {
+                errorString.setText("The spinner will only handle numbers.");
+            }
+            return null;
+        };
+        //declare integer-only TextField
+        passwordLength.setTextFormatter(new TextFormatter<Integer>(new IntegerStringConverter(), 0, integerFilter));
+
+        /*
+            - configure spinners to accept realistic resolutions using SpinnerValueFactory
+            - ( minValue==0 // maxValue==50 // startValue==1 // incrementValue==5 )
+        */
+        SpinnerValueFactory<Integer> numSpinnerFactory = new SpinnerValueFactory.IntegerSpinnerValueFactory(0, 50, 0, 5);
+        SpinnerValueFactory<Integer> uppSpinnerFactory = new SpinnerValueFactory.IntegerSpinnerValueFactory(0, 50, 0, 5);
+        maxNumCount.setValueFactory(numSpinnerFactory);
+        maxUpperCount.setValueFactory(uppSpinnerFactory);
+
+        TextField numSpinnerTextField = maxNumCount.getEditor();
+        TextField uppSpinnerTextField = maxUpperCount.getEditor();
+
+        //add listener to manNumCount and maxUpperCount to newly created text-fields.
+        numSpinnerTextField.setTextFormatter(new TextFormatter<Integer>(new IntegerStringConverter(), 0, integerFilter));
+        uppSpinnerTextField.setTextFormatter(new TextFormatter<Integer>(new IntegerStringConverter(), 0, integerFilter));
+    }
 
     @FXML
     void generateButton(ActionEvent event) {
         displayString.setText("");
         errorString.setText("");
 
-        String symbolInstance = symbolStringCreator(boxArray);
+        //populate object variables
+        int length = Integer.parseInt(passwordLength.getText());
+        boolean hasUppers = includeUppercase.isSelected();
+        boolean hasNumbers = includeNumbers.isSelected();
+        int upperMax = maxUpperCount.getValue();
+        int numberMax = maxNumCount.getValue();
+        String symbolString = symbolStringCreator(boxArray);
 
-        //set if app is use uppers and/or numbers in calculation
-        thePassword.setHasUppers(hasUppers.isSelected());
-        thePassword.setHasNumbers(hasNumbers.isSelected());
+        //instantiate new Password object and call function to generate password.
 
-        //method to call respective setters and return number
-        try {
-            int length = captureNumber(passwordLength);
-            int upCount = captureNumber(maxUpperCount);
-            int numCount = captureNumber(maxNumCount);
-
-            String result = thePassword.passwordCreator(length, upCount, numCount, symbolInstance);
-            displayString.setText(result);
-
-            //DEBUGGING PRINT LINES
-            System.out.printf("SYMBOL_STRING:   %s%n", symbolInstance);
-            System.out.printf("PASSWORD_OBJ:   %s%n", thePassword.toString());
-            System.out.printf("RETURN:   %s%n%n", result);
-
-        } catch (Exception ex) {
-            errorString.setText(ex.getMessage());
-        }
-
-
-
+        //debug print lines
+        System.out.println("Length of.. " + length);
+        System.out.printf("hasUppers is %s with %d%n", hasUppers, upperMax);
+        System.out.printf("hasNumbers is %s with %d%n", hasNumbers, numberMax);
+        System.out.printf("symbols... %n%s%n", symbolString);
     }
 
     @FXML
@@ -84,7 +110,6 @@ public class PasswordController implements Initializable {
             checkBox.setSelected(false);
         }
     }
-
     @FXML
     void selectAllButton(ActionEvent event) {
         //loop through the CheckBox[] -- set everything to true
@@ -93,6 +118,8 @@ public class PasswordController implements Initializable {
         }
     }
 
+
+    
     private void captureUserSymbols() {
         /*
             - to be called on start to populate the boxArray
@@ -132,65 +159,9 @@ public class PasswordController implements Initializable {
     }  //method to return checkBox variables as checkboxArray
 
     /**
-     * @param userInput
-     * used to filter out strings before calling appropriate setters.
-     *
-     * @exception NumberFormatException
-     * thrown if input is not found to be a number.
-     * */
-    private int captureNumber(TextField userInput) {
-        String inputType = userInput.getId();
-        switch(inputType) {
-            case "passwordLength":
-                if(userInput.getText().isEmpty()) {
-                    userInput.setText("0");
-                }
-                try {
-                    int len = Integer.parseInt(userInput.getText());
-                    thePassword.setLength(len);
-                } catch(NumberFormatException nfe) {
-                    throw new NumberFormatException("Password length must be a number.");
-                }
-                break;
-
-            case "maxUpperCount":
-                if(userInput.getText().isEmpty() && thePassword.isUsingUppers()) {
-                    userInput.setText("1");
-                }
-                if(userInput.getText().isEmpty() && !thePassword.isUsingUppers()) {
-                    userInput.setText("0");
-                }
-                try {
-                    int res = Integer.parseInt(userInput.getText());
-                    thePassword.setUpperMax(res);
-                } catch(NumberFormatException nfe) {
-                    throw new NumberFormatException("Uppercase limit must be a number.");
-                }
-                break;
-
-            case "maxNumCount":
-
-                if(userInput.getText().isEmpty() && thePassword.isUsingNumbers()) {
-                    userInput.setText("1");
-                }
-                if(userInput.getText().isEmpty() && !thePassword.isUsingNumbers()) {
-                    userInput.setText("0");
-                }
-                try {
-                    int res = Integer.parseInt(userInput.getText());
-                    thePassword.setNumberMax(res);
-                } catch(NumberFormatException fne) {
-                    throw new NumberFormatException("Number limit must be a number.");
-                }
-                break;
-        }
-        return Integer.parseInt(userInput.getText());
-    }
-
-    /**
      * @return
-     * String of user-desired symbols
-    * */
+     * string of user-desired symbols
+     */
     private String symbolStringCreator(CheckBox[] theBoxArray) {
 
         StringBuilder symbols = new StringBuilder();
@@ -200,6 +171,7 @@ public class PasswordController implements Initializable {
                 symbols.append(checkBox.getText());
             }
         }
+
 
         return symbols.toString();
     }
